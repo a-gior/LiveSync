1686c291a6559c19e4d2e3adbf8bbdd057c594dd
a-gior <alexis.giordan@orange.fr>
2024-04-19 18:18:41 +0200

    Fix Indentation

--- webviews/configuration/script.js
+++ webviews/configuration/script.js
@@ -11,11 +11,11 @@ let isFormValid = false;
 function toggleAuthMethod() {
     var authMethod = document.getElementById('auth-method').value;
     if (authMethod === 'password') {
-      document.getElementById('password-input').style.display = 'block';
-      document.getElementById('ssh-input').style.display = 'none';
+        document.getElementById('password-input').style.display = 'block';
+        document.getElementById('ssh-input').style.display = 'none';
     } else if (authMethod === 'ssh') {
-      document.getElementById('password-input').style.display = 'none';
-      document.getElementById('ssh-input').style.display = 'block';
+        document.getElementById('password-input').style.display = 'none';
+        document.getElementById('ssh-input').style.display = 'block';
     }
 }
 
