5e341e2a9fca4664d0f671130a2e96961057e7dd
a-gior <alexis.giordan@orange.fr>
2024-05-04 00:21:50 +0200

    Configure Remote Server - WIP Testing
    7. **Testing**:
        - [ ]  Conduct thorough testing of the remote server configuration functionality, including testing various authentication methods, connection scenarios, and error conditions.

--- src/test/webview-test/livesync.configuration.test.ts
+++ src/test/webview-test/livesync.configuration.test.ts
@@ -1,31 +1,23 @@
 import * as vscode from 'vscode';
-import chai from 'chai';
 import * as assert from 'assert';
-import { Workbench, EditorView, WebView, By } from 'vscode-extension-tester';
 
 suite('LiveSync Configuration Command Tests', () => {
     vscode.window.showInformationMessage('Start LiveSync configuration command tests.');
-    let view: WebView;
 
     suiteSetup(async function() {
         this.timeout(8000);
         // open a sample web view
-        await new Workbench().executeCommand('Configuration');
-        await new Promise((res) => { setTimeout(res, 500); });
-        // init the WebView page object
-        view = new WebView();
+        
+        console.log("suiteSetup");
 
-        console.log(view);
-
-        await view.switchToFrame();
     });
 
     test('TestConnection & Save Button', async () => {
         
 
         // now we can use findWebElement to look for elements inside the webview
-        const element = await view.findWebElement(By.css('h1'));
-        chai.expect(await element.getText()).has.string('This is a web view');
+
+        console.log("test");
 
         // it('Look for all elements with given locator', async () => {
         //     // analogically, findWebElements to search for all occurences
@@ -36,8 +28,8 @@ suite('LiveSync Configuration Command Tests', () => {
 
     suiteTeardown(async () => {
         // after we are done with the webview, switch webdriver back to the vscode window
-        await view.switchBack();
-        await new EditorView().closeAllEditors();
+
+        console.log("suiteTeardown");
     });
 
     // test('Save Button', async () => {
