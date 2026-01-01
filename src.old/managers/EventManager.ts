561a0fcd957da519ac3a817001662b96a6360c0f
a-gior <155831889+a-gior@users.noreply.github.com>
2026-01-01 03:15:26 +0100

    Remove old src files

--- src.old/managers/EventManager.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from "vscode";
-import { FileEventHandler } from "../services/FileEventHandler";
-import { SyncTreeDataProvider } from "../services/SyncTreeDataProvider";
-
-export class EventManager {
-  static initialize(context: vscode.ExtensionContext, treeDataProvider: SyncTreeDataProvider): void {
-    FileEventHandler.initialize(context, treeDataProvider);
-  }
-}
